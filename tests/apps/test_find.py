798a851c5d38d198be06ce69ee24ee9222a4c06c
Semyon Trifochkin <Gmasem@yandex.ru>
2024-01-25 22:35:48 +0200

    add: multithread find

--- tests/apps/test_find.py
+++ tests/apps/test_find.py
@@ -43,3 +43,14 @@ def test_find_with_sort(
     files = out.decode().rstrip('\n').split('\n')
     for i in range(0, len(files) - 1):
         assert files[i] < files[i+1]
+
+
+def test_find_in_file(
+    main_app,
+):
+    out, err = main_app.find('./fakes/fs', 'file_1.txt', sort=True, in_file='some_text_2')
+    assert len(err) == 0, err
+    files = out.decode().rstrip('\n').split('\n')
+    assert len(files) == 2
+    assert files[0] == os.path.join(path_to_fakes, 'file_1.txt')
+    assert files[1] == os.path.join(path_to_fakes, 'folder_1/folder_1_1/file_1.txt')
